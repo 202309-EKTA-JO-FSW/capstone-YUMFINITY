d514ff739f419e80b62b39971d50c407efc7350e
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2024-02-25 15:12:30 +0300

    created review model, with test

---
+++ server/server/Models/review.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+      // transform number to integer
+      get: (v) => Math.round(v),
+      set: (v) => Math.round(v),
+    },
+    comment: {
+      type: String,
+    },
+    reviewDate: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    restaurantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    orderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Review", reviewSchema);
