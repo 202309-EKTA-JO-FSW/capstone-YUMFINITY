0174e48d96564b51a3bdbb33bce0a08820107ada
belalninja <80969606+belalninja@users.noreply.github.com>
2024-02-26 21:11:11 +0300

    Public API GET restaurant list route and controller (#22)
    * added geolib dependency; will be used for location filtering
    
    * setup GET /restaurants route
    
    * Public API GET restaurant list route and controller
    closes #13

--- server/server/Routes/v1.js
+++ server/server/Routes/v1.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
 // we will put all of our major routes here
+router.use("/restaurants", require("./restaurant"));
 
 module.exports = router;
