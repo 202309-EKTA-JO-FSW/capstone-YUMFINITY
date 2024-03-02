2a210a3dda6dbdf59dd780296a041c4c954394fa
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2024-03-02 12:17:33 +0300

    enhance authentication error handling

--- server/server/Middlewares/passportAuthMiddleware.js
+++ server/server/Middlewares/passportAuthMiddleware.js
@@ -3,7 +3,10 @@ const passport = require("passport");
 module.exports = function passportAuthMiddleware(req, res, next) {
   passport.authenticate(["jwt"], (err, user, info) => {
     if (err) return res.status(500).json(err);
-    if (info) return res.status(401).json(info);
+    if (info)
+      return res.status(401).json({
+        error: { name: info[0].name, message: info[0].message, ...info[0] },
+      });
     if (user) req.user = user;
     return next();
   })(req, res, next);
