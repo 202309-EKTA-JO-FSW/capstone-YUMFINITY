1b6c831b64a7ec5269659499336aeca100f676f1
Dana AlSiddig <113601804+danasidd@users.noreply.github.com>
2024-03-03 16:28:53 +0300

    add customer controller- CRUD on user (#47)
    * add customer controller- CRUD on user
    
    * update updateCustomer by serializing and including specified fields
    
    * update updateCustomer and deleteCustomer

---
+++ server/server/Controllers/customerCrud.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+const User = require("../Models/user");
+
+const getCustomerById = async (req, res) => {
+  const id = req.user.userId;
+
+  try {
+    const customer = await User.findById(id).populate("cart");
+
+    if (!customer) {
+      return res.status(404).json({ message: "Customer not found" });
+    }
+
+    res.json(customer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+const updateCustomer = async (req, res) => {
+  try {
+      const id = req.user.userId;
+
+      const updatedFields = {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          phoneNumber: req.body.phoneNumber,
+          addresses: req.body.addresses,
+      };
+
+      const updatedCustomer = await User.findByIdAndUpdate(
+          id,
+          updatedFields,
+          { runValidators: true, new: true }
+      );
+
+      if (!updatedCustomer) {
+          return res.status(404).json({ message: "Customer not found" });
+      }
+
+      res.status(200).json({ message: "Customer updated successfully", updatedCustomer });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error.message });
+  }
+};
+
+
+const deleteCustomer = async (req, res) => {
+  const id = req.user.userId;
+
+  try {
+    const deletedCustomer = await User.findByIdAndDelete(id);
+
+    if (!deletedCustomer) {
+      return res
+      .clearCookie("refreshToken")
+      .clearCookie("accessToken")
+      .status(404).json({ message: "Customer not found" });    }
+
+    res.json({ message: "Customer deleted successfully", deletedCustomer });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getCustomerById,
+  updateCustomer,
+  deleteCustomer,
+};
