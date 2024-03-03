86c1a4f620cbe38597b19c64b2327821da318eac
Dana AlSiddig <danlsiddig@gmail.com>
2024-03-03 15:49:32 +0300

    update updateCustomer by serializing and including specified fields

--- server/server/Controllers/customerCrud.js
+++ server/server/Controllers/customerCrud.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const User = require("../Models/user");
 
 const getCustomerById = async (req, res) => {
-  const { id } = req.params;
+  const id = req.user.userId;
 
   try {
     const customer = await User.findById(id).populate("cart");
@@ -20,28 +20,36 @@ const getCustomerById = async (req, res) => {
 
 
 const updateCustomer = async (req, res) => {
-  const { id } = req.params;
-
   try {
-    const updatedCustomer = await User.findByIdAndUpdate(
-      id,
-      req.body,
-      { new: true }
-    );
+      const id = req.user.userId;
 
-    if (!updatedCustomer) {
-      return res.status(404).json({ message: "Customer not found" });
-    }
+      const updatedFields = {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          phoneNumber: req.body.phoneNumber,
+          addresses: req.body.addresses,
+      };
 
-    res.json({ message: "Customer updated successfully", updatedCustomer });
+      const updatedCustomer = await User.findByIdAndUpdate(
+          id,
+          updatedFields,
+          { runValidators: true, new: true }
+      );
+
+      if (!updatedCustomer) {
+          return res.status(404).json({ message: "Customer not found" });
+      }
+
+      res.status(200).json({ message: "Customer updated successfully", updatedCustomer });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error.message });
+      console.error(error);
+      res.status(500).json({ message: error.message });
   }
 };
 
+
 const deleteCustomer = async (req, res) => {
-  const { id } = req.params;
+  const id = req.user.userId;
 
   try {
     const deletedCustomer = await User.findByIdAndDelete(id);
