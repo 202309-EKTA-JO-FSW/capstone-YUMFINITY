6a31c793958387bbd0e2811f53a2433fcba19ba9
belalninja <80969606+belalninja@users.noreply.github.com>
2024-03-03 13:31:50 +0300

    admin routing and controller enhancement (#49)
    * enhance pagination util syntax
    
    * renamed admin controllers, format with prettier
    
    * configured admin routing
    
    * integrated authentication and authorization for admin routes

--- server/server/Routes/v1.js
+++ server/server/Routes/v1.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const passportAuthMiddleware = require("../Middlewares/passportAuthMiddleware");
+const authorizeAdmin = require("../Middlewares/authorizeAdmin");
 
 // we will put all of our major routes here
 
@@ -8,5 +10,13 @@ router.use("/", require("./restaurantRoutes"));
 
 // public API routes for sign in and sign up
 router.use("/", require("./signRoutes"));
+
+// admin specific routes
+router.use(
+  "/admin",
+  passportAuthMiddleware,
+  authorizeAdmin,
+  require("./adminRoutes"),
+);
 
 module.exports = router;
