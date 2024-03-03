4f372dc4350667e4016ecbe106563a34e1bfb561
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2024-03-03 07:54:03 +0300

    configured customer routing

--- server/server/Routes/v1.js
+++ server/server/Routes/v1.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const passportAuthMiddleware = require("../Middlewares/passportAuthMiddleware");
 
 // we will put all of our major routes here
 
@@ -8,5 +9,8 @@ router.use("/", require("./restaurantRoutes"));
 
 // public API routes for sign in and sign up
 router.use("/", require("./signRoutes"));
+
+// customer specific routes
+router.use("/", passportAuthMiddleware, require("./customerRoutes"));
 
 module.exports = router;
