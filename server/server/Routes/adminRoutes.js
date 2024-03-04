100c0e85839a59e2ec14c9bca4f832408c67f7cf
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2024-03-04 20:36:08 +0300

    removed unnecessery /:id from routes

--- server/server/Routes/adminRoutes.js
+++ server/server/Routes/adminRoutes.js
@@ -6,17 +6,14 @@ const adminController = require("../Controllers/adminController");
 // CRUD routes for items
 router.get("/item/:id", adminController.getItemById);
 router.post("/item/add", adminController.addNewItem);
-router.patch("/item/update/:id", adminController.updateItem);
-router.delete("/item/remove/:ids", adminController.removeOneOrManyItems);
+router.patch("/item/update", adminController.updateItem);
+router.delete("/item/remove", adminController.removeOneOrManyItems);
 
 // CRUD routes for restaurants
 router.get("/restaurant/:id", adminController.getRestaurantById);
 router.post("/restaurant/add", adminController.addnewRestaurant);
-router.patch("/restaurant/update/:id", adminController.updateRestaurant);
-router.delete(
-  "/restaurant/remove/:ids",
-  adminController.removeOneOrManyRestaurants,
-);
+router.patch("/restaurant/update", adminController.updateRestaurant);
+router.delete("/restaurant/remove", adminController.removeOneOrManyRestaurants);
 
 // CRUD routes for orders
 router.get("/current-orders", adminController.getDeliveringOrders);
