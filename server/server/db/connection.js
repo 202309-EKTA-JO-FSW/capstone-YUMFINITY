ac31663ed9905348ce1195c1395c6b2106bb7124
belalninja <80969606+belalninja@users.noreply.github.com>
2024-03-05 13:57:07 +0300

    Setup mongoDB Atlas connection (#61)
    Closes #60

--- server/server/db/connection.js
+++ server/server/db/connection.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, TEST_DB_HOST } =
   process.env;
 
-const DB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${
-  process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
-}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+const DB_URI =
+  process.env.NODE_ENV === "test"
+    ? `mongodb://${DB_USER}:${DB_PASSWORD}@${TEST_DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+    : process.env.MONGODB_ATLAS_URL;
 
 const url = DB_URI;
 
