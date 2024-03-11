181140c199928e507fbbab28e13af58c1605e231
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2024-03-11 08:57:47 +0300

    add services section

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,11 +1,13 @@
 import LandingPageNavbar from "./components/LandingPage/LandingPageNavbar";
 import Section from "./components/LandingPage/Section";
+import Services from "./components/LandingPage/Services";
 
 export default function Home() {
   return (
     <main className="relative flex flex-col items-center overflow-x-clip">
       <LandingPageNavbar />
       <Section />
+      <Services />
     </main>
   );
 }
