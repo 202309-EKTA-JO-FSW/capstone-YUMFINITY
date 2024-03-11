df324d2e7570c8be3348c32cfaf5bb32a6ffa9fc
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2024-03-11 11:16:45 +0300

    enhancements on restaurant card component

--- client/src/app/components/Restaurant/RestaurantCard.jsx
+++ client/src/app/components/Restaurant/RestaurantCard.jsx
@@ -9,36 +9,35 @@ export default function RestaurantCard({ className }) {
   const workingHours = ["Opened", "Closed"];
   const randomIndex = Math.floor(Math.random() * workingHours.length);
   return (
-    <div
-      className={`group relative flex flex-col justify-between rounded-xl border border-gray-400 bg-white leading-normal transition-all hover:[box-shadow:_0px_0px_10px_3px_#00000062] lg:border-gray-400 ${className}`}
+    <Link
+      href={"/"}
+      className={`group relative flex size-full flex-col justify-between rounded-xl bg-white leading-normal shadow-md shadow-gray-400 transition-all ${className}`}
     >
-      <Link href={"/"}>
-        <Image
-          src={restaurantPlaceholder}
-          priority
-          className="rounded-xl rounded-b-none"
-          alt="restuarant"
-        />
-        <div className="p-4 pt-3">
-          <div className="mb-2">
-            <div className="mb-2 flex items-center justify-between text-lg font-bold text-gray-900 hover:text-indigo-600">
-              Restaurant Name
-              <div className="flex items-center gap-1 text-sm">
-                {(Math.random() * (4 - 1 + 1) + 1).toFixed(1)}
-                <FaStar color="orange" />
-              </div>
+      <Image
+        src={restaurantPlaceholder}
+        priority
+        className="rounded-xl rounded-b-none"
+        alt="restuarant"
+      />
+      <div className="p-4 pt-5">
+        <div className="my-2 space-y-6">
+          <div className="mb-2 flex items-center justify-between text-pretty font-bold text-gray-900 hover:text-yellow-YUMFINITY">
+            Restaurant Name
+            <div className="flex items-center gap-1 text-sm">
+              {(Math.random() * (4 - 1 + 1) + 1).toFixed(1)}
+              <FaStar color="orange" />
             </div>
-            <p className="flex items-center gap-1 text-sm text-gray-700">
-              {workingHours[randomIndex] === "Opened" ? (
-                <TbClockHour5 className="size-5" />
-              ) : (
-                <TbEyeClosed className="size-5" />
-              )}
-              {workingHours[randomIndex]}
-            </p>
           </div>
+          <p className="flex items-center gap-1 text-sm text-gray-700">
+            {workingHours[randomIndex] === "Opened" ? (
+              <TbClockHour5 className="size-5" />
+            ) : (
+              <TbEyeClosed className="size-5" />
+            )}
+            {workingHours[randomIndex]}
+          </p>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
