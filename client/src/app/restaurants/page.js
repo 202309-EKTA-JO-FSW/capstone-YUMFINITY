d23d893f237de4f2adec38c70251d94271f726e6
janaabuhaltam <janaabuhaltam@gmail.com>
2024-03-14 00:15:16 +0300

    edit on restaurants list page

--- client/src/app/restaurants/page.js
+++ client/src/app/restaurants/page.js
@@ -1,20 +1,31 @@
+import Image from "next/image";
 import React from "react";
-import RestaurantCard from "../components/Restaurant/RestaurantCard";
+import allRestaurantsHeader from "./client/src/app/restaurants/allRestaurantsHeader.jpg";
+import { FaAnglesDown } from "react-icons/fa6";
 
-const AllReastaurant = () => {
+const Restaurants = () => {
   return (
     <div>
-      <div className="relative h-[900px] overflow-hidden bg-cover bg-[50%] bg-no-repeat">
-        <img src="/allRestaurantsHeader.jpg" alt="Example" />
+      <div className="relative flex h-[73vh] max-h-[710px] flex-col items-center justify-start shadow-lg dark:shadow-white/40">
+        <Image
+          src={allRestaurantsHeader}
+          priority={true}
+          className="absolute -z-10 size-full object-cover dark:brightness-90"
+          alt="Example"
+        />
 
-        <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed">
-          <div className=" px-6 text-center text-black md:px-12">
-            <h1 className="mb-6 mt-20 text-5xl font-bold">
+        <div className="h-full w-full">
+          <div className=" flex flex-col items-center px-6 text-center text-black md:px-12">
+            <h1 className="mb-4 mt-10 text-4xl font-bold sm:text-5xl">
               Explore Our Restaurants
             </h1>
-            <h3 className="mb-8 text-3xl ">
+            <h3 className="mb-4 text-xl sm:text-3xl ">
               Indulge in Speedy Satisfaction with Our Range of Food Options
             </h3>
+            <div className="flex items-center gap-3 text-lg">
+              Scroll down to browse Restaurants
+              <FaAnglesDown className="animate-bounce" />
+            </div>
           </div>
           <div className="flex h-full items-center justify-center"></div>
         </div>
@@ -25,13 +36,13 @@ const AllReastaurant = () => {
         </h2>
       </div>
       <div className="flex w-full justify-center">
-        <div className="flex- w-5/6  rounded-xl border border-gray-50 bg-white p-6 shadow-lg">
+        <div className="flex- w-5/6 rounded-xl bg-white p-6 shadow-lg dark:bg-white/50 dark:shadow-white/45">
           <form>
-            <div className="relative mb-10 flex w-full  items-center justify-between rounded-md">
+            <div className="relative mb-6 flex w-full items-center justify-between rounded-md">
               <input
                 type="name"
                 name="search"
-                className="h-12 w-full cursor-text rounded-md border border-gray-100 bg-gray-100 py-4 pl-12 pr-40 shadow-sm outline-none focus:border-[#FD7014] focus:ring focus:ring-[#FD7014] focus:ring-opacity-50"
+                className="h-12 w-full cursor-text rounded-md border border-gray-100 bg-gray-100 px-10 py-4 shadow-sm outline-none focus:border-[#FD7014] focus:ring focus:ring-[#FD7014] focus:ring-opacity-50 dark:text-black-YUMFINITY"
                 placeholder="Search by name, category, status, etc"
               />
             </div>
@@ -40,14 +51,14 @@ const AllReastaurant = () => {
               <div className="flex flex-col">
                 <label
                   htmlFor="manufacturer"
-                  className="text-sm font-medium text-stone-600"
+                  className="text-sm font-medium text-stone-600 dark:text-black-YUMFINITY"
                 >
                   Category
                 </label>
 
                 <select
                   id="manufacturer"
-                  className="mt-2 block w-full rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none focus:border-[#FD7014] focus:ring focus:ring-[#FD7014] focus:ring-opacity-50"
+                  className="mt-2 block w-full rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none focus:border-[#FD7014] focus:ring focus:ring-[#FD7014] focus:ring-opacity-50 dark:text-black-YUMFINITY"
                 >
                   <option>select</option>
                   <option>Indian</option>
@@ -61,14 +72,14 @@ const AllReastaurant = () => {
               <div className="flex flex-col">
                 <label
                   htmlFor="status"
-                  className="text-sm font-medium text-stone-600"
+                  className="text-sm font-medium text-stone-600 dark:text-black-YUMFINITY"
                 >
                   Status
                 </label>
 
                 <select
                   id="status"
-                  className="mt-2 block w-full cursor-pointer rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none focus:border-[#FD7014] focus:ring focus:ring-[#FD7014] focus:ring-opacity-50"
+                  className="mt-2 block w-full cursor-pointer rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none focus:border-[#FD7014] focus:ring focus:ring-[#FD7014] focus:ring-opacity-50 dark:text-black-YUMFINITY"
                 >
                   <option>select</option>
                   <option>open</option>
@@ -78,19 +89,18 @@ const AllReastaurant = () => {
             </div>
 
             <div className="mt-6 grid w-full grid-cols-2 justify-end space-x-4 md:flex">
-              <button className="rounded-lg bg-gray-200 px-8 py-2 font-medium text-[#FD7014] outline-none hover:opacity-80 focus:ring">
+              <button className="rounded-lg bg-gray-200 px-8 py-2 font-medium text-[#FD7014] outline-none hover:bg-gray-300 focus:ring">
                 Reset
               </button>
-              <button className="rounded-lg bg-[#FD7014] px-8 py-2 font-medium text-white outline-none hover:opacity-80 focus:ring">
+              <button className="rounded-lg bg-[#FD7014] px-8 py-2 font-medium text-white outline-none hover:bg-[#eb5d00] focus:ring">
                 Search
               </button>
             </div>
           </form>
         </div>
-        <RestaurantCard />
       </div>
     </div>
   );
 };
 
-export default AllReastaurant;
+export default Restaurants;
