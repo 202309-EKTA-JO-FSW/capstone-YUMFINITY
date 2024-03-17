e431db8863467cb950db3a9c5f66e407e1533220
belalninja <80969606+belalninja@users.noreply.github.com>
2024-03-17 22:21:40 +0300

    Configure Sign in integration with backend (#103)
    * create context provider to set user state universally; add js-cookie package
    
    * edit backend to send user data with sign in
    
    * configure user state inside general and homepage navbar
    
    * delete unnecessary function
    
    * configure Sign in page; fetch data and redirect; error handling

---
+++ client/src/app/SignIn/SignInForm.jsx
@@ -0,0 +1,121 @@
+"use client";
+
+import Link from "next/link";
+import GoogleSignIn from "./GoogleSignIn";
+import { useContext, useState } from "react";
+import { UserContext } from "../utils/contextProvider";
+import { redirect, useRouter } from "next/navigation";
+import styles from "./signin.module.css";
+
+export default function SignInForm({ submitData }) {
+  const { user, setUser } = useContext(UserContext);
+  const router = useRouter();
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const result = await submitData(formData);
+    if (!result) throw new Error("fetch failed");
+    if (!user) {
+      // localStorage.setItem("user", JSON.stringify(resultUser));
+      if (result.user) {
+        setUser(result);
+        router.push("/restaurants");
+      } else {
+        if (!error) setError(result.message);
+      }
+    }
+  }
+
+  function showPassword() {
+    setPasswordVisible(!passwordVisible);
+  }
+
+  if (user) return router.push("/restaurants");
+  return (
+    <form onSubmit={handleSubmit} className="relative">
+      {error && (
+        <div
+          onAnimationEnd={() => setError(null)}
+          className={`absolute z-10 rounded-md bg-red-400 py-5 ring-2 ring-red-YUMFINITY ${styles.Error}`}
+        >
+          <div className="pb-4 pl-10 text-left text-xl font-bold">Error:</div>
+          <div className="text-lg md:px-5">{error}</div>
+        </div>
+      )}
+      <div className="mb-10">
+        <h3 className=" text-3xl font-extrabold ">Sign in</h3>
+      </div>
+      <div className="mb-10 space-x-4 max-sm:space-y-4 sm:flex sm:items-start">
+        {/* <GoogleSignIn googleSignIn={googleSignIn} /> */}
+      </div>
+      <div>
+        <label className="mb-2 block text-sm">User name</label>
+        <div className="relative flex items-center">
+          <input
+            name="username"
+            type="text"
+            required
+            className="w-full rounded-md border border-gray-300 px-4 py-3 text-sm text-black-YUMFINITY outline-[#FD7014]"
+            placeholder="Enter user name"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#bbb"
+            stroke="#bbb"
+            className="absolute right-4 h-[18px] w-[18px]"
+            viewBox="0 0 24 24"
+          >
+            <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
+            <path
+              d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
+              data-original="#000000"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <div className="mt-6">
+        <label className="mb-2 block text-sm">Password</label>
+        <div className="relative flex items-center">
+          <input
+            name="password"
+            type={passwordVisible ? "text" : "password"}
+            required
+            className="w-full rounded-md border border-gray-300 px-4 py-3 text-sm text-black-YUMFINITY outline-[#FD7014]"
+            placeholder="Enter password"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#bbb"
+            stroke="#bbb"
+            className={`absolute right-3 size-7 cursor-pointer rounded-full p-1 transition-all hover:bg-slate-300 hover:fill-slate-800 ${passwordVisible ? "bg-yellow-YUMFINITY/20 fill-yellow-YUMFINITY" : ""}`}
+            viewBox="0 0 128 128"
+            onClick={showPassword}
+          >
+            <path
+              d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+              data-original="#000000"
+            ></path>
+          </svg>
+        </div>
+      </div>
+
+      <div className="mt-10">
+        <button className="w-full rounded bg-orange-400 px-4 py-2.5 text-sm font-semibold text-white shadow-xl transition-all hover:bg-orange-500 focus:outline-none active:bg-orange-900">
+          Log in
+        </button>
+      </div>
+      <p className="mt-6 text-center text-sm">
+        Do not have an account?{" "}
+        <Link
+          href="/SignUp"
+          className="ml-1 whitespace-nowrap font-semibold text-[#FD7014] hover:underline"
+        >
+          Register here
+        </Link>
+      </p>
+    </form>
+  );
+}
--- client/src/app/components/Navbar.jsx
+++ client/src/app/components/Navbar.jsx
@@ -1,10 +1,12 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { useContext, useState } from "react";
+import { usePathname } from "next/navigation";
 import Logo from "./Logo";
 import { FaBurger } from "react-icons/fa6";
+import { UserContext } from "../utils/contextProvider";
+import styles from "./Navbar.module.css";
 
 const navbarList = [
   {
@@ -38,159 +40,152 @@ const navbarList = [
 ];
 
 export default function Navbar() {
+  const { user, setUser } = useContext(UserContext);
   const path = usePathname();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userRole, setUserRole] = useState("");
-  const router = useRouter(); // Initialize useRouter
   const [isOpen, setOpen] = useState(false);
+  // const router = useRouter(); // Initialize useRouter
 
-  useEffect(() => {
-    let token = localStorage.getItem("token");
-    let userRole = localStorage.getItem("userRole");
-    let userID = localStorage.getItem("userID");
+  // useEffect(() => {}, []);
 
-    if (token) {
-      setIsLoggedIn(true);
-      setUserRole(userRole);
-    }
-  }, []);
+  // const handleLogin = () => {
+  //   setIsLoggedIn(true);
+  //   setUserRole("customer");
+  //   router.push("/");
+  // };
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-    setUserRole("customer");
-    router.push("/");
-  };
+  // const handleLogout = () => {
+  //   setIsLoggedIn(false);
+  //   setUserRole("");
+  //   localStorage.clear();
+  //   router.push("/");
+  // };
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setUserRole("");
-    localStorage.clear();
-    router.push("/");
-  };
+  if (path === "/") return null;
 
-  if (path !== "/")
-    return (
-      <header className="sticky left-0 top-0 z-10 w-full bg-yellow-YUMFINITY text-white-YUMFINITY shadow-lg duration-300 ease-in">
-        <nav className="mx-auto flex h-[4.5rem] max-w-[1366px] items-center justify-between bg-yellow-YUMFINITY md:h-[5.5rem] md:p-4">
-          <div>
-            <Link href="/">
-              <Logo className="h-16 w-14 flex-shrink-0 md:mr-7 md:h-24" />
-            </Link>
-          </div>
-          <div className="flex-shrink-0 font-boston text-3xl text-black-YUMFINITY md:hidden md:translate-x-16 md:text-5xl">
-            YUMFINITY
-          </div>
-          <aside
-            className={`peer relative flex text-black-YUMFINITY transition-all duration-300 ${isOpen ? "rotate-[360deg] text-white-YUMFINITY" : ""} md:hidden`}
-            onClick={() => setOpen(!isOpen)}
-          >
-            {/* <input
+  return (
+    <header
+      className={`sticky left-0 top-0 z-10 w-full bg-yellow-YUMFINITY text-white-YUMFINITY shadow-lg duration-300 ease-in ${styles.OVERFLOWYHIDDEN} ${isOpen ? styles.openedNavbar : ""}`}
+    >
+      <nav className="mx-auto flex h-[4.5rem] max-w-[1366px] items-center justify-between bg-yellow-YUMFINITY md:h-[5.5rem] md:p-4">
+        <div>
+          <Link href="/">
+            <Logo className="h-16 w-14 flex-shrink-0 md:mr-7 md:h-24" />
+          </Link>
+        </div>
+        <div className="flex-shrink-0 font-boston text-3xl text-black-YUMFINITY md:hidden md:translate-x-16 md:text-5xl">
+          YUMFINITY
+        </div>
+        <aside
+          className={`peer relative flex text-black-YUMFINITY transition-all duration-300 ${isOpen ? "rotate-[360deg] text-white-YUMFINITY" : ""} md:hidden`}
+          onClick={() => setOpen(!isOpen)}
+        >
+          {/* <input
               type="checkbox"
               name="burger-menu"
               id="burger-menu-toggle"
               className={`absolute inset-0 size-0`}
             /> */}
-            <label
-            // htmlFor="burger-menu-toggle"
-            >
-              <FaBurger
-                className="mx-3 size-10 flex-shrink-0 cursor-pointer rounded-full md:hidden"
-                id="burger-menu"
-              />
-            </label>
-          </aside>
-          <nav
-            className={`absolute right-0 top-[4.5rem] w-full transition-all duration-300 md:top-[5.5rem] ${isOpen ? "-translate-x-0" : "translate-x-full"}`}
+          <label
+          // htmlFor="burger-menu-toggle"
           >
-            <ul className="min-h-[calc(100vh-4rem)] space-y-5 bg-gradient-to-b from-black-YUMFINITY from-70% to-red-YUMFINITY text-white-YUMFINITY">
-              {navbarList.map(({ title, href }, index) => {
-                if (isLoggedIn && title.includes("Sign")) return null;
-                if (!isLoggedIn && title.includes("Profile")) return null;
-                return (
-                  <Link href={href} key={index}>
-                    <li
-                      className="border-b px-8 py-6 text-2xl hover:bg-slate-800"
-                      onClick={() => setOpen(!isOpen)}
-                    >
-                      {title}
-                    </li>
+            <FaBurger
+              className="mx-3 size-10 flex-shrink-0 cursor-pointer rounded-full md:hidden"
+              id="burger-menu"
+            />
+          </label>
+        </aside>
+        <nav
+          className={`absolute right-0 top-[4.5rem] w-full transition-all duration-300 md:top-[5.5rem] ${isOpen ? "-translate-x-0" : "translate-x-full"}`}
+        >
+          <ul className="min-h-[calc(100vh-4rem)] space-y-5 bg-gradient-to-b from-black-YUMFINITY from-70% to-red-YUMFINITY text-white-YUMFINITY">
+            {navbarList.map(({ title, href }, index) => {
+              if (user && title.includes("Sign")) return null;
+              if (!user && title.includes("Profile")) return null;
+              return (
+                <Link href={href} key={index}>
+                  <li
+                    className="border-b px-8 py-6 text-2xl hover:bg-slate-800"
+                    onClick={() => setOpen(!isOpen)}
+                  >
+                    {title}
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </nav>
+        {/*larger screen navigation*/}
+        <ul className="mr-auto hidden text-nowrap font-boston uppercase text-white-YUMFINITY md:flex lg:text-[20px]">
+          <li className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
+            <Link href="/contact-us">Contact Us</Link>
+          </li>
+        </ul>
+
+        <div className="hidden items-center md:flex">
+          {!user && (
+            <div className="flex gap-1">
+              <Link href={"/SignIn"}>
+                <button className="group relative me-1 inline-flex items-center justify-center overflow-hidden rounded-lg bg-red-600 bg-gradient-to-l p-0.5 text-black-YUMFINITY hover:text-white-YUMFINITY focus:outline-none active:bg-black-YUMFINITY">
+                  <span className="relative rounded-md bg-white-YUMFINITY px-2.5 py-1.5 text-xl transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-black-YUMFINITY">
+                    Sign In
+                  </span>
+                </button>
+              </Link>
+
+              <Link href={"/SignUp"}>
+                <button
+                  type="button"
+                  className=" mx-0.5 rounded-lg bg-red-600 bg-gradient-to-br px-2.5 py-2 text-center text-xl text-white-YUMFINITY hover:bg-red-YUMFINITY focus:outline-none active:bg-black-YUMFINITY"
+                >
+                  Sign Up
+                </button>
+              </Link>
+            </div>
+          )}
+
+          <ul className="hidden items-center font-boston uppercase text-white-YUMFINITY md:flex lg:text-[20px]">
+            {user && (
+              <>
+                <li>
+                  <Link href="/restaurants">
+                    <span className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
+                      Restaurants
+                    </span>
                   </Link>
-                );
-              })}
-            </ul>
-          </nav>
-          {/*larger screen navigation*/}
-          <ul className="mr-auto hidden text-nowrap font-boston uppercase text-white-YUMFINITY md:flex lg:text-[20px]">
-            <li className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
-              <Link href="/">Home</Link>
-            </li>
-            <li className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
-              <Link href="/about">About</Link>
-            </li>
-            <li className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
-              <Link href="/contact-us">Contact Us</Link>
-            </li>
+                </li>
+
+                <li className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
+                  <Link href={`/profile`}>Profile</Link>
+                </li>
+              </>
+            )}
           </ul>
 
-          <div className="hidden items-center md:flex">
-            {!isLoggedIn && (
-              <div className="flex gap-1">
-                <Link href={"/SignIn"}>
-                  <button className="group relative me-1 inline-flex items-center justify-center overflow-hidden rounded-lg bg-red-600 bg-gradient-to-l p-0.5 text-black-YUMFINITY hover:text-white-YUMFINITY focus:outline-none active:bg-black-YUMFINITY">
-                    <span className="relative rounded-md bg-white-YUMFINITY px-2.5 py-1.5 text-xl transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-black-YUMFINITY">
-                      Sign In
-                    </span>
-                  </button>
-                </Link>
+          <button className=" block text-white-YUMFINITY hover:text-gray-300 focus:outline-none md:hidden">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
 
-                <Link href={"/SignUp"}>
-                  <button
-                    type="button"
-                    className=" mx-0.5 rounded-lg bg-red-600 bg-gradient-to-br px-2.5 py-2 text-center text-xl text-white-YUMFINITY hover:bg-red-YUMFINITY focus:outline-none active:bg-black-YUMFINITY"
-                  >
-                    Sign Up
-                  </button>
-                </Link>
-              </div>
-            )}
-
-            <ul className="hidden items-center font-boston uppercase text-white-YUMFINITY md:flex lg:text-[20px]">
-              {isLoggedIn && (
-                <>
-                  <li>
-                    <Link href="/restaurants">
-                      <span className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
-                        Restaurants
-                      </span>
-                    </Link>
-                  </li>
-
-                  <li className="ml-2 rounded-md px-3 py-1 text-white-YUMFINITY focus:outline-none">
-                    <Link href={`/profile`}>Profile</Link>
-                  </li>
-                </>
-              )}
-            </ul>
-
-            <button className=" block text-white-YUMFINITY hover:text-gray-300 focus:outline-none md:hidden">
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          </div>
-
-          {/* <div className="absolute left-0 top-full z-10 hidden w-full bg-yellow-YUMFINITY md:hidden">
+        {/* <div className="absolute left-0 top-full z-10 hidden w-full bg-yellow-YUMFINITY md:hidden">
             <ul className="py-2">
               <li>
                 <Link href="/">
@@ -219,7 +214,7 @@ export default function Navbar() {
               )}
             </ul>
           </div> */}
-        </nav>
-      </header>
-    );
+      </nav>
+    </header>
+  );
 }
