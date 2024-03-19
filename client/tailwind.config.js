dfb31cc7a7398cadefa937236eb17b7d0ba38e6f
Dana Maraqa <dana.maraqa@hotmail.com>
2024-03-19 15:38:44 +0300

    Created About Page and connected to Navbar and Footer

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -21,7 +21,16 @@ module.exports = {
         "black-YUMFINITY": "#02020A",
         "white-YUMFINITY": "#F1F2EB",
       },
+      animation: {
+        "fade-in-up": "fadeInUp 0.5s ease-out forwards",
+      },
+      keyframes: {
+        fadeInUp: {
+          "0%": { opacity: 0, transform: "translateY(20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
     },
+    plugins: [],
   },
-  plugins: [],
 };
