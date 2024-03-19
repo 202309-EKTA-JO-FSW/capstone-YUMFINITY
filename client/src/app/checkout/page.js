0d271665dff72c2879dd35f258784a42e99e570f
JanaAbuHaltam <144922106+JanaAbuHaltam@users.noreply.github.com>
2024-03-20 01:49:36 +0300

    edit on checkout page (#116)

--- client/src/app/checkout/page.js
+++ client/src/app/checkout/page.js
@@ -11,7 +11,7 @@ const Checkout = () => {
         <div className="px-4 pt-8">
           <p className="text-xl font-medium">Order Summary</p>
           <p className="text-gray-400">
-            Check your items. And select a suitable shipping method.
+            Check your items. And select a suitable payment method.
           </p>
           <ShoppingCart />
           <p className="mt-8 text-lg font-medium">Choose payment method</p>
