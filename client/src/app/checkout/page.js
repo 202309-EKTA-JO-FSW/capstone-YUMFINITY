c1044c7316547c95033243d9e3cc8f4614578bbe
JanaAbuHaltam <144922106+JanaAbuHaltam@users.noreply.github.com>
2024-03-20 01:38:39 +0300

    created checkout page with payment methods (#106)
    * create checkout page with payment methods
    
    * edit checkout page payment method & payment details

---
+++ client/src/app/checkout/page.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ShoppingCart from "./ShoppingCart";
+import PaymentMethod from "./PaymentMethod";
+import PaymentForm from "./PaymentForm";
+
+const Checkout = () => {
+  return (
+    <div>
+      <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
+        <div className="px-4 pt-8">
+          <p className="text-xl font-medium">Order Summary</p>
+          <p className="text-gray-400">
+            Check your items. And select a suitable shipping method.
+          </p>
+          <ShoppingCart />
+          <p className="mt-8 text-lg font-medium">Choose payment method</p>
+          <PaymentMethod />
+        </div>
+        <PaymentForm />
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
