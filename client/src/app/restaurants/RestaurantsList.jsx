4d625ee4acc3bf27ff2b2935e46cc57be97c503c
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2024-03-21 00:44:07 +0300

    fetched restaurants; filtering functionality

---
+++ client/src/app/restaurants/RestaurantsList.jsx
@@ -0,0 +1,24 @@
+// import { useEffect, useState } from "react";
+import RestaurantCard from "../components/Restaurant/RestaurantCard";
+
+//   // Fetch restaurants when the component mounts.
+//   useEffect(() => {
+//     async function getRestaurantsList() {
+//       const { results } = await fetchRestaurants();
+//       if (results) setRestaurants(results);
+//     }
+//     getRestaurantsList();
+//   }, []);
+export default function RestaurantsList({ restaurants }) {
+  return (
+    <article className="my-16 grid gap-10 px-10 md:grid-cols-2 md:px-20 lg:grid-cols-3 xl:grid-cols-4">
+      {restaurants?.length > 0 &&
+        restaurants.map((res) => <RestaurantCard key={res._id} data={res} />)}
+      {(!restaurants || restaurants?.length === 0) && (
+        <div className="col-span-2 col-start-2 my-12 w-full text-center font-boston text-3xl">
+          No Results
+        </div>
+      )}
+    </article>
+  );
+}
--- client/src/app/restaurants/page.js
+++ client/src/app/restaurants/page.js
@@ -2,10 +2,21 @@ import Image from "next/image";
 import React from "react";
 import allRestaurantsHeader from "./allRestaurantsHeader.jpg";
 import { FaAnglesDown } from "react-icons/fa6";
+import SearchAndList from "./SearchAndList";
 
-const Restaurants = () => {
+async function fetchRestaurants(query) {
+  "use server";
+
+  const result = await fetch(
+    `http://192.168.100.5:3001/v1/restaurants?${query}`,
+  );
+  const data = await result.json();
+  return data;
+}
+
+const Restaurants = async () => {
   return (
-    <div>
+    <main>
       <div className="relative flex h-[73vh] max-h-[710px] flex-col items-center justify-start overflow-y-hidden shadow-lg dark:shadow-white/40">
         <Image
           src={allRestaurantsHeader}
@@ -13,7 +24,6 @@ const Restaurants = () => {
           className="absolute -top-10 -z-10 size-full min-h-[750px] object-cover dark:brightness-90"
           alt="Example"
         />
-
         <div className="h-full w-full">
           <div className=" flex flex-col items-center px-6 text-center text-black md:px-12">
             <h1 className="mb-4 mt-10 text-4xl font-bold sm:text-5xl">
@@ -32,74 +42,11 @@ const Restaurants = () => {
       </div>
       <div className="mx-auto my-10 max-w-screen-xl  px-4 ">
         <h2 className="font-b text-center text-2xl sm:text-5xl">
-          All Restaurants
+          Search Restaurants
         </h2>
       </div>
-      <div className="flex w-full justify-center">
-        <div className="flex- w-5/6 rounded-xl bg-white p-6 shadow-lg dark:bg-white/50 dark:shadow-white/45">
-          <form>
-            <div className="relative mb-6 flex w-full items-center justify-between rounded-md">
-              <input
-                type="name"
-                name="search"
-                className="h-12 w-full cursor-text rounded-md border border-gray-100 bg-gray-100 px-10 py-4 shadow-sm outline-none focus:border-[#FD7014] focus:ring focus:ring-[#FD7014] focus:ring-opacity-50 dark:text-black-YUMFINITY"
-                placeholder="Search by name, category, status, etc"
-              />
-            </div>
-
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              <div className="flex flex-col">
-                <label
-                  htmlFor="manufacturer"
-                  className="text-sm font-medium text-stone-600 dark:text-black-YUMFINITY"
-                >
-                  Category
-                </label>
-
-                <select
-                  id="manufacturer"
-                  className="mt-2 block w-full rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none focus:border-[#FD7014] focus:ring focus:ring-[#FD7014] focus:ring-opacity-50 dark:text-black-YUMFINITY"
-                >
-                  <option>select</option>
-                  <option>Indian</option>
-                  <option>Mid Eastern</option>
-                  <option>Italian</option>
-                  <option>Asian</option>
-                  <option>healthy</option>
-                </select>
-              </div>
-
-              <div className="flex flex-col">
-                <label
-                  htmlFor="status"
-                  className="text-sm font-medium text-stone-600 dark:text-black-YUMFINITY"
-                >
-                  Status
-                </label>
-
-                <select
-                  id="status"
-                  className="mt-2 block w-full cursor-pointer rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none focus:border-[#FD7014] focus:ring focus:ring-[#FD7014] focus:ring-opacity-50 dark:text-black-YUMFINITY"
-                >
-                  <option>select</option>
-                  <option>open</option>
-                  <option>closed</option>
-                </select>
-              </div>
-            </div>
-
-            <div className="mt-6 grid w-full grid-cols-2 justify-end space-x-4 md:flex">
-              <button className="rounded-lg bg-gray-200 px-8 py-2 font-medium text-[#FD7014] outline-none hover:bg-gray-300 focus:ring">
-                Reset
-              </button>
-              <button className="rounded-lg bg-[#FD7014] px-8 py-2 font-medium text-white outline-none hover:bg-[#eb5d00] focus:ring">
-                Search
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+      <SearchAndList fetchRestaurants={fetchRestaurants} />
+    </main>
   );
 };
 
