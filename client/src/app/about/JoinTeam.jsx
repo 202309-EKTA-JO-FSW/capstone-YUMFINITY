d36ba43ae47ea6c51a13bd6c04fbb177fedbfdcf
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2024-03-24 12:34:06 +0300

    add shadow to join team component

--- client/src/app/about/JoinTeam.jsx
+++ client/src/app/about/JoinTeam.jsx
@@ -19,10 +19,10 @@ export default function JoinTeam() {
   return (
     <div
       ref={ref}
-      className={`flex h-full items-center justify-center  dark:bg-gray-800 ${isVisible ? "animate-fade-in-up" : "opacity-0"}`}
+      className={`flex h-full items-center justify-center dark:bg-gray-800 ${isVisible ? "animate-fade-in-up" : "opacity-0"}`}
     >
       <div className="p-6">
-        <div className="mx-auto flex w-full max-w-5xl flex-wrap items-center rounded-xl border border-gray-200 bg-white p-5 text-left md:p-8 lg:flex-nowrap dark:border-gray-700">
+        <div className="mx-auto flex w-full max-w-5xl flex-wrap items-center rounded-xl border border-gray-200 bg-white p-5 text-left shadow-lg md:p-8 lg:flex-nowrap dark:border-gray-700">
           <div className="mb-5 w-full flex-1 md:mb-0 md:w-1/2 md:pr-5 lg:pr-10">
             <h3 className="mb-2 font-boston text-2xl text-gray-700 dark:text-gray-200">
               Join Yumfinity Team
