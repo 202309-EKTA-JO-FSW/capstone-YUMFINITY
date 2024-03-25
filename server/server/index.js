42f4c4f25fc2d8642cb165418bc31a90d2a3f70e
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2024-03-25 10:43:46 +0300

    edit cors for backend

--- server/server/index.js
+++ server/server/index.js
@@ -14,7 +14,7 @@ const port =
     ? process.env.NODE_LOCAL_TEST_PORT
     : process.env.PORT;
 
-app.use(cors());
+app.use(cors({ origin: "https://yumfinity.vercel.app", credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
